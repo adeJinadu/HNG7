773226d8218d6d19ec826e2703d7a23ac2833e1e
Ade <45346565+adeJinadu@users.noreply.github.com>
2020-06-04 20:50:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
-print "Hello world!<br>";
+print "Hello world! <br>";
 print "This is Ade Jinadu, with with HNGi7 ID 00217 using PHP for stage 2 task";
 ?> 
+
+</body>
+</html>
